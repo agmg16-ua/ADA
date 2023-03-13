d4a0ac6faab6cef8195fa522aa598c227910f344
agmg16-ua <agmg16@alu.ua.es>
2023-03-13 20:20:33 +0100

    Continuacion

--- Prac 5/pow2.cc
+++ Prac 5/pow2.cc
@@ -34,24 +34,32 @@ unsigned long pow2_2(unsigned n) {
 }
 
 unsigned long pow2_3(unsigned n) {
-    unsigned long resultado;
+
     pasos++;
-
-    if(n == 0) {
-        resultado = 1;
-    } else if(n%2 == 0) {
-        resultado = pow2_3(n/2) + pow2_3(n/2);
+    if (n == 0) {
+        return 1;
     } else {
-        resultado = 2*pow2_3(n-1);
-    }
 
-    return resultado;
+        // Calculamos la potencia para la mitad del exponente
+        int aux = pow2_3(n/2);
+        
+        // Si el exponente es par, devolvemos el cuadrado de la potencia para la mitad
+        if (n % 2 == 0) {
+            return aux * aux;
+        } else {  // Si el exponente es impar, devolvemos la potencia para la mitad por 2
+            return 2 * aux * aux;
+        }
+    }
+    
+
+
 }
 
 int main() {
     unsigned long n;
     //Con intencion de hacer la gráfica, se van a utilizar los exponentes 10, 25, 50 y 100
-    cout << "n    pow2_1  pow2_2------------pow2_3" << endl;
+    cout << "n    pow2_1  pow2_2   pow2_3" << endl
+        << "--------------------------------------" << endl;
     for(n = 0; n<=20; n++) {
 
         cout << n << "    \t";
@@ -75,7 +83,7 @@ int main() {
         //pow2_3
         pasos = 0;
         if(pow2_3(n) != pow(2, n)) {
-            cerr << "Panic, diferent result of pow(2, " << n << ')' << pow2_3(n) << endl;
+            cerr << "Panic, diferent result of pow(2, " << n << ')' << endl;
             exit(EXIT_FAILURE);
         }       
         cout << pasos << endl;
