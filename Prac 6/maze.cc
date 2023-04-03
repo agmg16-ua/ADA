7134663c01ee575437c0e64c9841676ddb4f158f
agmg16-ua <agmg16@alu.ua.es>
2023-04-03 14:44:31 +0200

    Arreglado el desastre de sobreescritura y comenzado a plantear el maze_it_vector

--- Prac 6/maze.cc
+++ Prac 6/maze.cc
@@ -131,7 +131,67 @@ int maze_naive(vector<vector<int>>& matriz, unsigned row, unsigned col) {
     return min(min(derecha, abajo), diag) + 1;
 } 
 
-vector<vector<int>> maze_memo(vector<vector<int>> &matriz) {
+int maze_memo(vector<vector<int>> matriz, vector<vector<int>> &auxiliar, int row, int col) {
+    int filas = matriz.size();
+    int columnas = matriz[0].size();
+    
+    if(auxiliar[row][col] != -1) {
+        return auxiliar[row][col];
+    }
+
+    if(matriz[filas-1][columnas-1] == 0 || matriz[0][0] == 0) {
+        auxiliar[row][col] = 0;
+        return auxiliar[row][col];
+    }
+
+    if(matriz[row][col] == 0) {
+        auxiliar[row][col] = INT_MAX-1;
+        return auxiliar[row][col];
+    }
+
+    if(row == filas-1 && col == columnas-1) {
+        auxiliar[row][col] = 1;
+        return auxiliar[row][col];
+    }
+
+    if(row == filas-1) {
+        int der = maze_memo(matriz, auxiliar, row, col+1);
+        if(der == INT_MAX-1) {
+            auxiliar[row][col] = der;
+            return auxiliar[row][col];
+        }
+
+        auxiliar[row][col] = der + 1;
+        return auxiliar[row][col];
+    }
+
+    if(col == columnas-1) {
+        int abajo = maze_memo(matriz, auxiliar, row+1, col);
+        if(abajo == INT_MAX-1) {
+            auxiliar[row][col] = abajo;
+            return auxiliar[row][col];
+        }
+
+        auxiliar[row][col] = abajo + 1;
+        return auxiliar[row][col];
+    }
+
+    int der = maze_memo(matriz, auxiliar, row, col+1);
+    int abajo = maze_memo(matriz, auxiliar, row+1, col);
+    int diag = maze_memo(matriz, auxiliar, row+1, col+1);
+
+    int result = min(der, min(abajo, diag));
+
+    if(result == INT_MAX - 1) {
+        auxiliar[row][col] = result;
+        return auxiliar[row][col];
+    }
+
+    auxiliar[row][col] = result + 1;
+    return auxiliar[row][col];
+}
+
+vector<vector<int>> maze_it_matrix(vector<vector<int>> &matriz) {
     int n = matriz.size();
     int m = matriz[0].size();
     vector<vector<int>> auxiliar(n, vector<int>(m, -1));
@@ -181,17 +241,23 @@ vector<vector<int>> maze_memo(vector<vector<int>> &matriz) {
     return auxiliar;
 }
 
-void maze_it_matrix() {
+int maze_it_vector(vector<vector<int>> matriz, vector<int> anterior, int row) {
+    int n = matriz.size();
+    int m = matriz[0].size();
 
+    return 0;
 }
 
-void maze_it_vector() {
-
+void maze_parser() {
+    cout << "?" <<  endl;
 }
+
+//-------------------------------------------IMPRIMIR TODO-----------------------------------------------------------------------------
 
 //Se usa para imprimir los datos en pantalla, y tambien calculará el camino con '*' utilizando una funcion auxiliar
-void maze_parser(int argc, char* argv[], vector<vector<int>> file_matrix, int naive, vector<vector<int>> memo_table) {
-    int memo = memo_table[0][0] + 1;
+void print_all(int argc, char* argv[], vector<vector<int>> file_matrix, int naive, vector<vector<int>> memo_table, int memo, vector<vector<int>> it_matrix, int it_vector) {
+    memo = memo_table[0][0];
+    int it1 = it_matrix[0][0]+1;
 
     if(naive == -1) {
         cout << '-';
@@ -201,20 +267,18 @@ void maze_parser(int argc, char* argv[], vector<vector<int>> file_matrix, int na
 
     cout << ' ';
 
-    cout << memo << " ? ?" << endl;
+    cout << memo << " " << it1 << " " << it_vector << endl;
 
     if(param_position(argc, argv, "-p") != -1) {
-        /*int contador = memo-1;
-        print_path(file_matrix, memo_table, contador);*/
-        cout << "?" << endl;
+        maze_parser();
     }
 
     if(param_position(argc, argv, "-t") != -1) {
         cout << "Memoization table:" << endl;
         for(unsigned i = 0; i < file_matrix.size(); i++) {
             for(unsigned j = 0; j < file_matrix[0].size(); j++) {
-                if(memo_table[i][j] >= 0 && memo_table[i][j] < memo) {
-                    cout << setw(5) << memo - memo_table[i][j];
+                if(memo_table[i][j] >= 0 && memo_table[i][j] <= memo) {
+                    cout << setw(5) << memo - memo_table[i][j] + 1;
                 } else if(memo_table[i][j] < 0) {
                     cout << setw(5) << "X";
                 } else {
@@ -226,13 +290,24 @@ void maze_parser(int argc, char* argv[], vector<vector<int>> file_matrix, int na
         }
 
         cout << "Iterative table:" << endl;
-        cout << "?" << endl;
+        for(unsigned i = 0; i < file_matrix.size(); i++) {
+            for(unsigned j = 0; j < file_matrix[0].size(); j++) {
+                if(it_matrix[i][j] >= 0 && it_matrix[i][j] < memo) {
+                    cout << setw(5) << memo - it_matrix[i][j];
+                } else if(it_matrix[i][j] < 0) {
+                    cout << setw(5) << "X";
+                } else {
+                    cout << setw(5) << "-";
+                }
+            
+            }
+            cout << endl;
+        }
     } 
 
-
-    cout << endl;
-
 }
+
+//-----------------------------------------------------------------------------------------------------
 
 
 
@@ -246,26 +321,45 @@ int main(int argc, char* argv[]) {
     vector<vector<int>> file_matrix = get_data_file(argv[file_position]);
 
     //maze_naive-------------------------------------------------------------------------------
+
     if(param_position(argc, argv, "--ignore-naive") == -1) {
         naive = maze_naive(file_matrix, 0, 0);
     } else {
         naive = -1;
     }
 
-    //maze_mamo-------------------------------------------------------------------------------
-    vector<vector<int>> aux_memo(file_matrix.size(), vector<int>(file_matrix[0].size()));
+    //maze_memo-------------------------------------------------------------------------------
 
-    for(unsigned i=0; i<aux_memo.size(); i++) {
-        for(unsigned j=0; j<aux_memo[0].size(); j++) {
-            aux_memo[i][j] = file_matrix[i][j];
+    vector<vector<int>> memo_table(file_matrix.size(), vector<int>(file_matrix[0].size()));
+
+    for(unsigned i=0; i<memo_table.size(); i++) {
+        for(unsigned j=0; j<memo_table[0].size(); j++) {
+            memo_table[i][j] = -1;
         }
     }
     
-    vector<vector<int>> memo_table = maze_memo(aux_memo);
+    int memo = maze_memo(file_matrix, memo_table, 0, 0);
+
+    //maze_it_matrix---------------------------------------------------------------------------
+
+    vector<vector<int>> it_matrix(file_matrix.size(), vector<int>(file_matrix[0].size()));
+    
+    for(unsigned i=0; i<it_matrix.size(); i++) {
+        for(unsigned j=0; j<it_matrix[0].size(); j++) {
+            it_matrix[i][j] = file_matrix[i][j];
+        }
+    }
+
+    it_matrix = maze_it_matrix(it_matrix);
+
+    //maze_it_vector----------------------------------------------------------------------------
+    vector<int> anterior = vector<int>(file_matrix[0]);
+
+    int it_vector = maze_it_vector(file_matrix, anterior, 0);
     
     //print-------------------------------------------------------------------------------------
 
-    maze_parser(argc, argv, file_matrix, naive, memo_table);
+    print_all(argc, argv, file_matrix, naive, memo_table, memo, it_matrix, it_vector);
 
     return 0;
 }
